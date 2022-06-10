f07a81852faffb79a6a8efa9e04334191c49f78c
Ranjan2703 <95168243+Ranjan2703@users.noreply.github.com>
2022-06-10 16:38:36 +0530

    Time: 209 ms (63.92%), Space: 9.1 MB (45.07%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        
+        //vector for updating the dp array/values
+        vector<int> dp(n+1,INT_MAX);
+        //base case
+        
+        dp[0]=0;
+        int num = 1;
+        
+        while(num*num <= n) {
+            
+        int sq = num*num;
+            
+        for(int i = sq; i < n+1; i++) {
+            
+            dp[i] = min(dp[i-sq] + 1,dp[i]);
+        }
+        num++;
+    }
+    return dp[n];
+    }
+};
