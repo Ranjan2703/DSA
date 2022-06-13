7b9ca10ecf5c46a48bec3952a6113d5adb567efc
Ranjan2703 <95168243+Ranjan2703@users.noreply.github.com>
2022-06-13 14:12:35 +0530

    Added solution - LeetHub

--- Minimum number of deletions. - GFG/minimum-number-of-deletions..cpp
+++ Minimum number of deletions. - GFG/minimum-number-of-deletions..cpp
@@ -19,21 +19,23 @@ return 0;
 }// } Driver Code Ends
 
 
-int minDeletions(string str, int n) { 
-    //complete the function here 
-    string str2 = str;
-    reverse(str.begin(), str.end());
-    int dp[n+1][n+1];
+int minDeletions(string s, int n) { 
+    //complete the function here
+    string t= s;
+    reverse(t.begin(), t.end());
     
-    for(int i = 0; i <= n; i++){
-        for(int j = 0; j <= n; j++){
-            if(i == 0 || j == 0)
-                dp[i][j] = 0;
-            else if(str[i-1] == str2[j-1])
-                dp[i][j] = 1 + dp[i-1][j-1];
-            else
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-        }
+    int dp[n+1][n+1];
+    for(int i=0;i<n+1;i++)
+    {
+         for(int j=0;j<n+1;j++)
+         {
+              if(i == 0 || j==0) dp[i][j] =0;
+              else if(s[i-1] == t[j-1])
+              dp[i][j] = dp[i-1][j-1] +1;
+              
+              else
+               dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+         }
     }
     
     return n - dp[n][n];
